6d15d96b8cc05021f4459764f55e34eb6c2e5edf
Dhiman Dasgupta <dhiman.dasgupta1984@gmail.com>
2019-07-31 03:14:19 +0530

    Day 5. Added Coroutine dependencies. Adding to List and modified list works are done in IO and Default Coroutine contexts, for smoother ui.

--- app/src/main/java/com/dhiman/complexrecycler/MainActivityViewModel.kt
+++ app/src/main/java/com/dhiman/complexrecycler/MainActivityViewModel.kt
@@ -3,7 +3,13 @@ package com.dhiman.complexrecycler
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dhiman.complexrecycler.model.*
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
     private val list: MutableList<BaseParent> = mutableListOf()
@@ -14,50 +20,64 @@ class MainActivityViewModel : ViewModel() {
     val listLiveData: LiveData<List<BaseParent>> = mutableListLiveData
 
     init {
-        for (i in 0..50) {
-            if (i % 2 == 0) {
-                list.add(
-                    ParentOne(
-                        i,
-                        false,
-                        listOf(
-                            ChildOne(i, "Item $i"),
-                            ChildTwo(i + 1, "Item ${(i + 1)}"),
-                            ChildOne(i + 2, "Item ${(i + 2)}"),
-                            ChildTwo(i + 3, "Item ${(i + 3)}")
+        viewModelScope.launch(IO) {
+            for (i in 0..50) {
+                if (i % 2 == 0) {
+                    list.add(
+                        ParentOne(
+                            i,
+                            false,
+                            listOf(
+                                ChildOne(i, "Item $i"),
+                                ChildTwo(i + 1, "Item ${(i + 1)}"),
+                                ChildOne(i + 2, "Item ${(i + 2)}"),
+                                ChildTwo(i + 3, "Item ${(i + 3)}")
+                            )
                         )
                     )
-                )
-            } else {
-                list.add(
-                    ParentTwo(
-                        i,
-                        false,
-                        listOf(
-                            ChildOne(i + 4, "Item $i"),
-                            ChildTwo(i + 5, "Item ${(i + 1)}"),
-                            ChildOne(i + 6, "Item ${(i + 2)}"),
-                            ChildTwo(i + 7, "Item ${(i + 3)}")
+                } else {
+                    list.add(
+                        ParentTwo(
+                            i,
+                            false,
+                            listOf(
+                                ChildOne(i + 4, "Item $i"),
+                                ChildTwo(i + 5, "Item ${(i + 1)}"),
+                                ChildOne(i + 6, "Item ${(i + 2)}"),
+                                ChildTwo(i + 7, "Item ${(i + 3)}")
+                            )
                         )
                     )
-                )
+                }
+            }
+
+            viewModelScope.launch(Main) {
+                mutableListLiveData.postValue(list)
             }
         }
+    }
 
-        mutableListLiveData.postValue(list)
+    override fun onCleared() {
+        super.onCleared()
+
+        viewModelScope.cancel()
     }
 
     fun onCollapseExpandClicked(baseParent: BaseParent) {
-        val newList = list.map {
-            val collapsed = if (it.id != baseParent.id) it.collapsed else !baseParent.collapsed
-            when (it) {
-                is ParentOne -> it.copy(collapsed = collapsed)
-                is ParentTwo -> it.copy(collapsed = collapsed)
-            }
-        }.toList()
+        viewModelScope.launch(Default) {
+            val newList = list.map {
+                val collapsed = if (it.id != baseParent.id) it.collapsed else !baseParent.collapsed
+                when (it) {
+                    is ParentOne -> it.copy(collapsed = collapsed)
+                    is ParentTwo -> it.copy(collapsed = collapsed)
+                }
+            }.toList()
 
-        list.clear()
-        list.addAll(newList)
-        mutableListLiveData.postValue(list)
+            viewModelScope.launch(Main) {
+                list.clear()
+                list.addAll(newList)
+                mutableListLiveData.postValue(list)
+            }
+        }
     }
 }
